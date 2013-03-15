797db132d25f7b5ac8c4e74e6f2f8d387366f0dc
Dan Cameron <dan@sproutventure.com>
2013-03-15 15:41:53 -0700

    No credits for purchases using credits.

--- classes/gbsRewards.class.php
+++ classes/gbsRewards.class.php
@@ -7,27 +7,43 @@ class GB_Affiliates_Ext {
 	private static $instance;
 
 	private function add_addon() {
-		add_filter( 'gb_addons', array( $this, 'load_custom_addons' ), 10, 1 );
+		add_filter( 'gb_addons', array( $this, 'load_custom_addons' ), 1000, 1 ); // Load later than others
 	}
 
 	public static function load_custom_addons( $addons ) {
-		$addons['new_user_affiliate_credits'] = array(
-			'label' => __( 'Affiliate Credits Restrictions' ),
-			'description' => __( 'Only apply credits to affiliates when the purchase was from a new user. This modification may interfere with the Deal Based Rewards Add-on.' ),
+		$addons['affiliate_credit_restrictions_pwc'] = array(
+			'label' => __( 'Affiliate Credits Restrictions (purchases without credits)' ),
+			'description' => __( 'Only apply credits if the purchase used credits. This requires "Deal Based Rewards" to be loaded.' ),
 			'files' => array(
 				__FILE__,
 				dirname( __FILE__ ) . '/library/template-tags.php',
 			),
 			'callbacks' => array(
-				array( __CLASS__, 'add_hooks' ),
+				array( __CLASS__, 'add_hooks_pwc' ),
+			),
+		);
+		$addons['affiliate_credit_restrictions_pwc'] = array(
+			'label' => __( 'Affiliate Credits Restrictions (14-day delay) - IN DEVELOPMENT' ),
+			'description' => __( 'Delay all credits 14 days to verify if voucher is active.' ),
+			'files' => array(
+				__FILE__,
+				dirname( __FILE__ ) . '/library/template-tags.php',
+			),
+			'callbacks' => array(
+				array( __CLASS__, 'add_hooks_delay' ),
 			),
 		);
 		return $addons;
 	}
 
-	private function add_hooks() {
+	private function add_hooks_pwc() {
+		// Filter Deal Based rewards
+		add_filter( 'gb_dbr_prevent_credits_from_a_credit_purchase', '__return_true' );
+	}
+
+	private function add_hooks_delay() {
 		remove_action( 'gb_apply_credits', array( 'Group_Buying_Notifications', 'applied_credits' ), 10, 4 );
-		add_action( 'gb_apply_credits', array( get_class(), 're_apply_credits' ), 10, 1 );
+		add_action( 'gb_apply_credits', array( get_class(), 'delay_credits' ), 10, 1 );
 	}
 
 	////////////////
@@ -35,22 +51,29 @@ class GB_Affiliates_Ext {
 	////////////////
 
 	
-	public static function re_apply_credits( Group_Buying_Account $affiliate_account, Group_Buying_Payment $payment, $applied_credits, $credit_type ) {
+	public static function delay_credits( Group_Buying_Account $affiliate_account, Group_Buying_Payment $payment, $applied_credits, $credit_type ) {
 		
 		// Start off by removing the credit that was just applied.
 		$affiliate_account->deduct_credit( $applied_credits, $credit_type );
-		// Get the registration date.
-		$purchaser_account = $payment->get_account();
-		$purchaser_registration_time = get_the_time( $purchaser_account->get_id() );
 
-		// Check to see if the registration date plus the buffer is before the current time. 
-		if ( time() > ( $purchaser_registration_time + self::NEW_REGISTRATION_TIME ) ) {
-			// apply the credits back
-			$affiliate_account->add_credit( $applied_credits, $credit_type );
-			do_action( 'gb_apply_credits_with_reg_restriction', $affiliate_account, $payment, $applied_credits, $credit_type );
-			// Fire off the notification manually
-			Group_Buying_Notifications::applied_credits( $affiliate_account, $payment, $applied_credits, $credit_type );
-		}
+		// Create a record of the delayed credit
+		
+	}
+
+	public function find_delayed_credits() {
+		// run a query to find all the delayed credits
+	}
+
+	public function maybe_apply_credit( Group_Buying_Account $affiliate_account, Group_Buying_Payment $payment, $applied_credits, $credit_type ) {
+
+		// Loop through all the vouchers associated with the payment
+		
+		// Check to make sure the voucher is active
+		// apply the credits back
+		$affiliate_account->add_credit( $applied_credits, $credit_type );
+		do_action( 'gb_apply_credits_with_reg_restriction', $affiliate_account, $payment, $applied_credits, $credit_type );
+		// Fire off the notification manually
+		Group_Buying_Notifications::applied_credits( $affiliate_account, $payment, $applied_credits, $credit_type );
 	}
 
 	/********** Singleton *************/
