a9fbe993c88ca58ea58e363589fa6d4930d5e2df
Dan Cameron <dan@sproutventure.com>
2013-03-15 16:15:08 -0700

    Only apply credits if the purchase used credits and allow for merchants to edit the credit field.

--- classes/gbsRewards.class.php
+++ classes/gbsRewards.class.php
@@ -7,22 +7,22 @@ class GB_Affiliates_Ext {
 	private static $instance;
 
 	private function add_addon() {
-		add_filter( 'gb_addons', array( $this, 'load_custom_addons' ), 1000, 1 ); // Load later than others
+		add_filter( 'gb_addons', array( $this, 'load_custom_addons' ), 5, 1 ); // Load later than others
 	}
 
 	public static function load_custom_addons( $addons ) {
 		$addons['affiliate_credit_restrictions_pwc'] = array(
-			'label' => __( 'Affiliate Credits Restrictions (purchases without credits)' ),
-			'description' => __( 'Only apply credits if the purchase used credits. This requires "Deal Based Rewards" to be loaded.' ),
+			'label' => __( 'Deal Based Rewards Modification' ),
+			'description' => __( 'Only apply credits if the purchase used credits and allow for merchants to edit the credit field. This requires "Deal Based Rewards" to be loaded.' ),
 			'files' => array(
 				__FILE__,
 				dirname( __FILE__ ) . '/library/template-tags.php',
 			),
 			'callbacks' => array(
-				array( __CLASS__, 'add_hooks_pwc' ),
+				array( __CLASS__, 'add_pwc_hooks' ),
 			),
 		);
-		$addons['affiliate_credit_restrictions_pwc'] = array(
+		$addons['affiliate_credit_restrictions_delay'] = array(
 			'label' => __( 'Affiliate Credits Restrictions (14-day delay) - IN DEVELOPMENT' ),
 			'description' => __( 'Delay all credits 14 days to verify if voucher is active.' ),
 			'files' => array(
@@ -30,18 +30,23 @@ class GB_Affiliates_Ext {
 				dirname( __FILE__ ) . '/library/template-tags.php',
 			),
 			'callbacks' => array(
-				array( __CLASS__, 'add_hooks_delay' ),
+				array( __CLASS__, 'add_delay_hooks' ),
 			),
 		);
 		return $addons;
 	}
 
-	private function add_hooks_pwc() {
-		// Filter Deal Based rewards
+	public function add_pwc_hooks() {
+		// prevent credits when a purchase uses credits
 		add_filter( 'gb_dbr_prevent_credits_from_a_credit_purchase', '__return_true' );
+
+		// Merchant Options
+		add_filter( 'gb_deal_submission_fields', array( get_class(), 'filter_deal_submission_fields' ), 10, 1 );
+		add_action( 'submit_deal',  array( get_class(), 'submit_deal' ), 10, 1 );
+		add_action( 'edit_deal',  array( get_class(), 'submit_deal' ), 10, 1 );
 	}
 
-	private function add_hooks_delay() {
+	public function add_delay_hooks() {
 		remove_action( 'gb_apply_credits', array( 'Group_Buying_Notifications', 'applied_credits' ), 10, 4 );
 		add_action( 'gb_apply_credits', array( get_class(), 'delay_credits' ), 10, 1 );
 	}
@@ -74,6 +79,42 @@ class GB_Affiliates_Ext {
 		do_action( 'gb_apply_credits_with_reg_restriction', $affiliate_account, $payment, $applied_credits, $credit_type );
 		// Fire off the notification manually
 		Group_Buying_Notifications::applied_credits( $affiliate_account, $payment, $applied_credits, $credit_type );
+	}
+
+	/**
+	 * Filter the merchant submission page
+	 *
+	 * @param array   $fields
+	 * @return array
+	 */
+	public function filter_deal_submission_fields( $fields ) {
+		global $wp; // get_query_var won't work at this point
+		$default = '';
+		if ( isset( $wp->query_vars[Group_Buying_Deals_Edit::EDIT_DEAL_QUERY_VAR] ) ) {
+			$deal_id = $wp->query_vars[Group_Buying_Deals_Edit::EDIT_DEAL_QUERY_VAR];
+			$deal = Group_Buying_Deal::get_instance( $deal_id );
+			$default = Group_Buying_Deal_Reward::get_credits( $deal );
+		}
+		$fields['dbr_credit'] = array(
+			'label' => gb__( 'Credits' ),
+			'weight' => 17,
+			'type' => 'text',
+			'default' => $default,
+			'required' => FALSE,
+			'description' => gb__( 'How many credits should the affiliate get.' )
+		);
+		return $fields;
+	}
+
+	/**
+	 * Save the credit field after the submission
+	 *
+	 * @param Group_Buying_Deal $deal
+	 * @return
+	 */
+	public function submit_deal( Group_Buying_Deal $deal ) {
+		$credit = isset( $_POST['gb_deal_dbr_credit'] ) ? $_POST['gb_deal_dbr_credit'] : '';
+		Group_Buying_Deal_Reward::set_credits( $deal, $credit );
 	}
 
 	/********** Singleton *************/
