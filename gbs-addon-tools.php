3a98caa12ff7e6a6998421c32e5273c53f6b1de3
Dan Cameron <dan@sproutventure.com>
2013-03-25 17:31:16 -0700

    version update.

--- gbs-addon-tools.php
+++ gbs-addon-tools.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Group Buying Addon - GBS Tools
-Version: 0.8
+Version: 0.8.1
 Description: Multiple Add-ons and Customizations for the the GBS platform.
 Plugin URI: http://groupbuyingsite.com/
 Author: GroupBuyingSite.com
